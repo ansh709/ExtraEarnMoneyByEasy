9b0cd3d89b2129bf6761fcc195a5ec7de2ff148c
venue <82924899+ansh709@users.noreply.github.com>
2021-05-21 12:53:24 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -6,4 +6,35 @@ window.addEventListener('load',function(){
 	document.querySelector('.later').addEventListener('click',function(){
 		document.querySelector('.add-to').style.display ="none";
 	})
+    
+	setTimeout(function(){
+		document.querySelector('#signupForm').style.display ="block";
+	},7000);
+
+
 })
+
+function prohold(){
+	(profilePage.style.display ==="none")?profilePage.style.display = "block" : profilePage.style.display = "none"
+	
+}
+backfromprofile.addEventListener('click',function(){
+		profilePage.style.display ="none";
+})
+
+submit.addEventListener('click',function(){
+	var s_name = document.querySelector("[name='fullname']").value;
+	var s_email = document.querySelector("[name='email']").value;
+	var s_phone = document.querySelector("[name='phone']").value;
+	var s_pass = document.querySelector("[name='password']").value;
+	console.log(s_name);
+	
+	myname.innerHTML= s_name;
+	email.innerHTML += s_email;
+	phone.innerHTML+= s_phone;
+	password.innerHTML+= s_pass;
+	profilePage.style.display = "block"
+	signupForm.style.display="none";	
+})
+
+
